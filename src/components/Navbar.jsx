ad99a2cc9be85b7ab38182df8fb5c5d2a7627ac1
Sean Wang <seanwaterloo2997@gmail.com>
2024-05-18 15:20:16 -0400

    Navbar positioning

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="sticky top-0 w-full flex-wrap items-center justify-between bg-zinc-50 py-2 shadow-dark-mild dark:bg-neutral-700 lg:py-4">
+    <nav className="top-0 w-full flex-wrap items-center justify-between bg-zinc-50 py-2 shadow-dark-mild dark:bg-neutral-700">
       <div className="flex w-full flex-wrap items-center justify-between px-3">
         <a className="text-xl text-black dark:text-white" href="#">Cover Letter Generator</a>
       </div>
