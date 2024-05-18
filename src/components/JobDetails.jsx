d692fb4e8a693e6846fd8ca85a3e0edbc35c1563
Sean Wang <seanwaterloo2997@gmail.com>
2024-05-18 17:57:49 -0400

    More styling

--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -3,34 +3,32 @@ import React from 'react'
 const JobDetails = () => {
     
     return (
-        <form className="w-full">
-        <div className="">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">Job Details</h2>
-    
-          <div className="mt-6">
-            <div className="sm:col-span-3">
-              <label for="job-title" className="block text-sm font-medium leading-6 text-gray-900">Job Title</label>
-              <div className="mt-2">
-                <input type="text" name="job-title" id="job-title" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
-              </div>
+      <div className="w-full">
+        <h2 className="text-base font-semibold leading-7 text-gray-900">Job Details</h2>
+  
+        <div className="mt-6">
+          <div className="sm:col-span-3">
+            <label for="job-title" className="block text-sm font-medium leading-6 text-gray-900">Job Title</label>
+            <div className="mt-2">
+              <input type="text" name="job-title" id="job-title" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
             </div>
-    
-            <div className="sm:col-span-3">
-              <label for="company-name" className="block text-sm font-medium leading-6 text-gray-900">Company Name</label>
-              <div className="mt-2">
-                <input type="text" name="company-name" id="company-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
-              </div>
+          </div>
+  
+          <div className="sm:col-span-3">
+            <label for="company-name" className="block text-sm font-medium leading-6 text-gray-900">Company Name</label>
+            <div className="mt-2">
+              <input type="text" name="company-name" id="company-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
             </div>
-    
-            <div className="sm:col-span-4">
-              <label for="jobdescription" className="block text-sm font-medium leading-6 text-gray-900">Job Description</label>
-              <div className="mt-2">
-                <textarea name="jobdescription" id="jobdescription" rows="5" className="block w-full rounded-md border-0 py-2 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 resize-none overflow-auto"></textarea>
-              </div>
+          </div>
+  
+          <div className="sm:col-span-4">
+            <label for="jobdescription" className="block text-sm font-medium leading-6 text-gray-900">Job Description</label>
+            <div className="mt-2">
+              <textarea name="jobdescription" id="jobdescription" rows="5" className="block w-full rounded-md border-0 py-2 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 resize-none overflow-auto"></textarea>
             </div>
           </div>
         </div>
-      </form>
+      </div>
     );
   };
 
--- src/components/ReviewProfile.jsx
+++ src/components/ReviewProfile.jsx
@@ -2,42 +2,38 @@ import React from 'react'
 
 const ReviewProfile = () => {
   return (
-<form className="w-full">
-  <div className="space-y-12">
-    <div>
-      <h2 className="text-lg font-semibold leading-7 text-gray-900">Profile</h2>
+  <div className="w-full">
+    <h2 className="text-lg font-semibold leading-7 text-gray-900">Profile</h2>
 
-      <div className="mt-6">
-        <div className="sm:col-span-3">
-          <label for="first-name" className="block text-sm font-medium leading-6 text-gray-900">First name</label>
-          <div className="mt-2">
-            <input type="text" name="first-name" id="first-name" autocomplete="given-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
-          </div>
+    <div className="mt-6">
+      <div className="sm:col-span-3">
+        <label for="first-name" className="block text-sm font-medium leading-6 text-gray-900">First name</label>
+        <div className="mt-2">
+          <input type="text" name="first-name" id="first-name" autocomplete="given-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
         </div>
+      </div>
 
-        <div className="sm:col-span-3">
-          <label for="last-name" className="block text-sm font-medium leading-6 text-gray-900">Last name</label>
-          <div className="mt-2">
-            <input type="text" name="last-name" id="last-name" autocomplete="family-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
-          </div>
+      <div className="sm:col-span-3">
+        <label for="last-name" className="block text-sm font-medium leading-6 text-gray-900">Last name</label>
+        <div className="mt-2">
+          <input type="text" name="last-name" id="last-name" autocomplete="family-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
         </div>
+      </div>
 
-        <div className="sm:col-span-4">
-          <label for="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-          <div className="mt-2">
-            <input id="email" name="email" type="email" autocomplete="email" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
-          </div>
+      <div className="sm:col-span-4">
+        <label for="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+        <div className="mt-2">
+          <input id="email" name="email" type="email" autocomplete="email" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
         </div>
-        <div className="sm:col-span-4">
-          <label for="workexperience" className="block text-sm font-medium leading-6 text-gray-900">Work Experience</label>
-          <div className="mt-2">
-            <textarea name="workexperience" id="workexperience" autocomplete="workexperience" rows="5" className="block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 resize-none overflow-auto"></textarea>
-          </div>
+      </div>
+      <div className="sm:col-span-4">
+        <label for="workexperience" className="block text-sm font-medium leading-6 text-gray-900">Work Experience</label>
+        <div className="mt-2">
+          <textarea name="workexperience" id="workexperience" autocomplete="workexperience" rows="5" className="block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 resize-none overflow-auto"></textarea>
         </div>
       </div>
     </div>
   </div>
-</form>
 
   )
 }
